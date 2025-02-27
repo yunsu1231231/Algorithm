847ce78d16466cf682f392ea83b9aeb4a5ad9caa
Yunsu <pianoest@naver.com>
2025-02-27 22:04:53 +0900

    2025_02_27 파이썬 기초공부

--- Sweeping Algorithm/test.py
+++ Sweeping Algorithm/test.py
@@ -1,32 +1,9 @@
-N = map(int,input().split())
+a = [1, 2, 3]
 
+# 리스트 자체의 주소값
+print(id(a))  # 리스트 객체 자체의 메모리 주소
 
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
-    mid = int(len(arr) / 4)
-    
-    left = arr[:mid]
-    right = arr[mid:]
-    left_sorted = mergeSort(left)
-    right_sorted = mergeSort(right)
-    
-    return merge(left_sorted, right_sorted)
-
-def merge(left, right):
-    result = []
-    i, j = 0, 0
-
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    result += left[i:]
-    result += right[j:]
-    return result
-
-for _ in mergeSort(chicken):
-    print(_, end = " ")
+# 각 요소의 주소값
+print(id(a[0]))  # 첫 번째 요소 (1)의 메모리 주소
+print(id(a[1]))  # 두 번째 요소 (2)의 메모리 주소
+print(id(a[2]))  # 세 번째 요소 (3)의 메모리 주소
